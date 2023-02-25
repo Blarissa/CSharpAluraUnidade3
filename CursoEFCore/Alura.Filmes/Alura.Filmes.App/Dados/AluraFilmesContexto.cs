68018259a623b8412d9a987d5cdc034df51551d1
Blarissa <larissabrasil009@gmail.com>
2023-02-25 18:16:43 -0300

    feat:  adicionando contexto

---
+++ CursoEFCore/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)mssqllocaldb;Database=AluraFilmes;Trusted_connection=true;");
+        }
+    }    
+}
