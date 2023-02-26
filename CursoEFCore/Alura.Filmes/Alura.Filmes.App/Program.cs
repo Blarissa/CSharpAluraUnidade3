0120aff8911588af23af862c2693418d10491680
Blarissa <larissabrasil009@gmail.com>
2023-02-26 20:10:00 -0300

    feat: exibindo atores

--- CursoEFCore/Alura.Filmes/Alura.Filmes.App/Program.cs
+++ CursoEFCore/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+
+namespace Alura.Filmes.App
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                foreach (var ator in contexto.Atores)
+                {
+                    Console.WriteLine(ator);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
