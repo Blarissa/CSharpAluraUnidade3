8f2a8e697d6cef42551183c4147a79020407177b
Blarissa <larissabrasil009@gmail.com>
2023-02-27 01:19:48 -0300

    refactor: retirando DataAnnotations

--- CursoEFCore/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
+++ CursoEFCore/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +9,9 @@ using System.Threading.Tasks;
 namespace Alura.Filmes.App.Negocio
 {
     public class Ator
-    {
+    {   
         public int Id { get; set; }
-        public string PrimeiroNome { get; set; }
+        public string PrimeiroNome { get; set; }        
         public string UltimoNome { get; set; }
     }
 }
