008d41f2581a028a7283155f7692b6b0a8a92e8a
Blarissa <larissabrasil009@gmail.com>
2023-02-27 11:49:08 -0300

    feat: configurando ToString

--- CursoEFCore/Alura.Filmes/Alura.Filmes.App/Negocio/Filme.cs
+++ CursoEFCore/Alura.Filmes/Alura.Filmes.App/Negocio/Filme.cs
@@ -12,5 +12,11 @@ namespace Alura.Filmes.App.Negocio
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public string AnoLancamento { get; set; }
+        public short Duracao { get; set; }
+
+        public override string ToString()
+        {
+            return $"Filme ({Id}): {Titulo} = {AnoLancamento}";
+        }
     }
 }
